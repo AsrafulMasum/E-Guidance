843bc0c3ac0ff3032bffa4dabfe8e5cbe7ec32c1
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2024-01-05 21:05:54 +0600

    login and signup page fully complete

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -109,7 +109,7 @@ const LogIn = () => {
           </div>
 
           <div className="mt-6">
-            <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-700 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+            <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-700 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 flex justify-center">
             {loading ? (
                   <ImSpinner9 className="animate-spin text-lg"></ImSpinner9>
                 ) : (
